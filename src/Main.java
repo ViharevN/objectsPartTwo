df7d108f8455ef4da8d19f27d44e39ff60a5e5bb
ViharevN <swix_off@mail.ru>
2022-10-27 17:35:52 +0300

    домашнее задание методы объектов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
+
+
     public static void main(String[] args) {
+        Author author = new Author("Mark", "Twain");
+        System.out.println("author.name = " + author.getFirstName());
+        System.out.println("author.secondName = " + author.getLastName());
+
+        Book book = new Book("Tom Sayer", author, 1876);
+        System.out.println("book.name = " + book.getName());
+        System.out.println("book.author = " + book.getAuthor());
+        System.out.println("book.year = " + book.getYear());
+        book.setYear(1999);
+        System.out.println("book.year = " + book.getYear());
+
+
+
+        Author author1 = new Author("Alexander", "Pushkin");
+        Book book1 = new Book("Golden fish", author1, 1833);
+        book1.setYear(13_10_1833);
+        System.out.println("book1.getYear() = " + book1.getYear());
+        System.out.println("book1.getAuthor() = " + book1.getAuthor());
+        System.out.println("book1.toString() = " + book1.toString());
+        System.out.println("book1.equals(book) = " + book1.equals(book));
+        System.out.println("book1.hashCode() = " + book1.hashCode());
+
 
     }
 }
